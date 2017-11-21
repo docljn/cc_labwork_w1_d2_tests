cd1e9ca70193fef6c5b950d09cc056cc9055b7eb
DocLJN <ljnoble@gmail.com>
2017-11-21 21:56:26 +0000

    Calculate volume of sphere

--- specs/my_functions_spec.rb
+++ specs/my_functions_spec.rb
@@ -63,16 +63,18 @@ class MyFunctionsTest < MiniTest::Test
 # #
     #Further
 
-    #Given the length of a side of a cube calculate the volume
-    def test_volume_of_cube()
-      result = volume_of_cube(2)
-      assert_equal(8, result)
-    end
+    # #Given the length of a side of a cube calculate the volume
+    # def test_volume_of_cube()
+    #   result = volume_of_cube(2)
+    #   assert_equal(8, result)
+    # end
 
-#     #Given the radius of a sphere calculate the volume
-#     def test_volume_of_sphere()
-#       #add test code here
-#     end
+    #Given the radius of a sphere calculate the volume
+    def test_volume_of_sphere()
+      result = volume_of_sphere(2).round(2)
+      assert_equal(25.13, result)
+      #add test code here
+    end
 #
 #     #Given a value in farenheit, convert this into celsius.
 #     def test_fahrenheit_to_celsius()
